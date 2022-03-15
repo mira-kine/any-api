5bc15d215fcfa38a85dd9ac319c5377425ed563d
mira-kine <mirak0419@gmail.com>
2022-03-15 16:57:36 -0700

    update by id passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const { insert } = require('../lib/models/Deepsea');
+const { insert, getById } = require('../lib/models/Deepsea');
 
 describe('any-api routes', () => {
   beforeEach(() => {
@@ -35,5 +35,27 @@ describe('any-api routes', () => {
         diet: 'plankton',
       },
     ]);
+  });
+
+  it('should get animals by id', async () => {
+    const animal = await insert({ name: 'Flapjack Octopus', diet: 'plankton' });
+    const res = await request(app).get(`/api/v1/deepsea/${animal.id}`);
+
+    expect(res.body).toEqual(animal);
+  });
+
+  it('should update animal by id', async () => {
+    const animal = await insert({ name: 'Flapjack Octopus', diet: 'plankton' });
+    const res = await request(app)
+      .patch(`/api/v1/deepsea/${animal.id}`)
+      .send({ name: 'Flapjack', diet: 'creatures' });
+
+    const expected = {
+      id: expect.any(String),
+      name: 'Flapjack',
+      diet: 'creatures',
+    };
+    expect(res.body).toEqual(expected);
+    expect(await getById(animal.id)).toEqual(expected);
   });
 });
